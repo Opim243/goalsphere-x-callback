52128330ea36c3a10391c2917f9f8c20230a3372
Opim243 <jesmop3@gmail.com>
2026-09-17 02:52:31 +0100

    app.py

--- app.py
+++ app.py
@@ -179,9 +179,4 @@ if __name__ == "__main__":
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)        "has_refresh_token": bool(token.get("refresh_token"))
-    }
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)     
